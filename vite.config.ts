5173d11ab6e561cd1b89c43da5d5ec5051387630
mhmdasm628-netizen <mhmdasm628@gmail.com>
2026-03-16 15:32:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
 })
